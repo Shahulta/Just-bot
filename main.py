4df2e217a52d6ef72df24b0744f9c443300f7b70
Shahulta <131171791+Shahulta@users.noreply.github.com>
2023-05-01 08:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyrogram import Client, filters
 
+
+
+Bot=Client(
+    "Pyrogram_bot"
+    bot_token="6004112610:AAF8AMEnrDIlQDDUu68PFrjApLu9UzfYmCU",
+    api_id="17296819",
+    api_hash="baa838bcf579f71dbcd5ef2ab9ca8f2b"
+)
+
+
+@Bot.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("hi")
+
+
+Bot.run()
